cd946a3f32403b32373a4ba0a87d9869c84a34fd
“arshagarwal” <arshagarwal.18@gmail.com>
2022-07-03 04:57:08 +0000

    adding env_stup code

---
+++ set_env.sh
@@ -0,0 +1,5 @@
+# downloads packages and sets environment for testing
+pip install gdown
+mkdir checkpoints
+gdown https://drive.google.com/u/0/uc?id=1pB4mufFtzbJSxxv_2iFrBPD3vp_Ef-n3&export=download
+unzip /content/DLFS/males_model.zip -d checkpoints
